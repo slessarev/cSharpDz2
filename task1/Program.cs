ca251dafc73c63ab829996d76339a401f8f20af8
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-05 08:36:12 +0300

    Применил плагин для красивого отображения текста

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.Clear();
 Console.Write("Введите трёхзначное число: ");
-int num=int.Parse(Console.ReadLine());
-Console.Write("Вторая цифра числа "+ num + ": " + num/10%10);
+int num = int.Parse(Console.ReadLine());
+Console.Write("Вторая цифра числа " + num + ": " + num / 10 % 10);
