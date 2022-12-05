00e078eb56f916be7e58dc5ddd2c1b7a32df17f9
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-05 08:59:39 +0300

    Решил вторую задачу

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100) Console.Write("Третьей цифры у числа " + num + " нет.");
+else
+{
+    while (num > 1000) num = num / 10;
+    Console.Write("Третья цифра у введенного числа: " + num % 10);
+}
