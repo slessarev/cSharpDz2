5289807d5569468f0317b198ef5bc95fcb4399f9
Aleksandr Slesarev <slessarevalex@ya.ru>
2022-12-05 08:30:52 +0300

    Решил первую задачу

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int num=int.Parse(Console.ReadLine());
+Console.Write("Вторая цифра числа "+ num + ": ");
+Console.Write(num/10%10);
